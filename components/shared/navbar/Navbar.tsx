6d6ccfb440838f66c89601b3667a2d256c4987a5
Sumit Kumar <106050212+sumit957602@users.noreply.github.com>
2024-08-12 22:49:46 +0530

    update

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,10 +1,10 @@
-import { SignedIn, UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import Theme from './Theme'
-import MobileNav from './MobileNav'
-import GlobalSearch from '../search/GlobalSearch'
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
@@ -17,7 +17,9 @@ const Navbar = () => {
           alt="DevFlow"
         />
 
-        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">Dev <span className="text-primary-500">Overflow</span></p>
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Code <span className="text-primary-500">QueryHub</span>
+        </p>
       </Link>
 
       <GlobalSearch />
@@ -26,15 +28,15 @@ const Navbar = () => {
         <Theme />
 
         <SignedIn>
-          <UserButton 
+          <UserButton
             afterSignOutUrl="/"
             appearance={{
               elements: {
-                avatarBox: 'h-10 w-10'
+                avatarBox: "h-10 w-10",
               },
               variables: {
-                colorPrimary: '#ff7000'
-              }
+                colorPrimary: "#ff7000",
+              },
             }}
           />
         </SignedIn>
@@ -42,7 +44,7 @@ const Navbar = () => {
         <MobileNav />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
